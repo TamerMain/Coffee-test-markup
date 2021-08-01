507c5e6e2f24e4a7712ec68b90ffbb1104b15122
TamerMain <arthur.tamer.main@gmail.com>
2021-08-02 00:45:09 +0300

    Carousel + Button Styling

--- src/Components/ItemList/ItemList.js
+++ src/Components/ItemList/ItemList.js
@@ -1,10 +1,13 @@
+import Carousel, { arrowsPlugin, Dots, slidesToShowPlugin } from "@brainhubeu/react-carousel";
+import "@brainhubeu/react-carousel/lib/style.css";
+
 import WidthWrapper from "../Common/WidthWrapper";
-import useInnerWidth from '../../hooks/useInnerWidth';
+import useInnerWidth from "../../hooks/useInnerWidth";
 
 import Item from "./Item";
 import classes from "./ItemList.module.css";
-import rightArrow from "../../SVG/Arrow 1.svg";
-import leftArrow from "../../SVG/Arrow 2.svg";
+import rightArrowImage from "../../SVG/Arrow 1.svg";
+import leftArrowImage from "../../SVG/Arrow 2.svg";
 
 const coffee = [
   {
@@ -22,31 +25,79 @@ const coffee = [
     description:
       "Espresso - black coffee made in a coffee machine or a special coffee machine, where hot water is passed through pressed ground beans at a pressure.",
   },
+  {
+    name: "Arabica",
+    description:
+      "You may have seen bags of coffee labeled `100 percent Arabica.` Arabica is the most popular type of bean used for coffee. ",
+  },
+  {
+    name: "Robusta",
+    description:
+      "Robusta beans are typically cheaper to produce because the Robusta plant is easier to grow. They have a higher caffeine content and taste more bitter than Arabica beans.",
+  },
+  {
+    name: "Decaf",
+    description:
+      "Coffee beans naturally contain caffeine, but roasters can use several different processes to remove almost all of it",
+  },
+  {
+    name: "Cappuccino",
+    description:
+      "This espresso-based drink is similar to a latte, but the frothy top layer is thicker.",
+  },
+  {
+    name: "Macchiato",
+    description:
+      "A macchiato is a shot of espresso with just a touch of steamed milk or foam.",
+  },
+  {
+    name: "Café au Lait",
+    description:
+      "This fancy-sounding French drink is actually super simple: It's equal parts coffee and steamed or scalded milk.",
+  },
+  
 ];
 
 function ItemList() {
   const [currentWidth] = useInnerWidth();
 
   let sliceSize;
-  if (currentWidth > 1300) {sliceSize = 3};
-  if (currentWidth < 1300) {sliceSize = 2};
-  if (currentWidth < 992) {sliceSize = 1};
-  
+  if (currentWidth > 1300) {
+    sliceSize = 3;
+  }
+  if (currentWidth < 1300) {
+    sliceSize = 2;
+  }
+  if (currentWidth < 992) {
+    sliceSize = 1;
+  }
+
+  const leftArrow = currentWidth < 400 ? <></> : <button className={classes.carouselArrows}><img src={leftArrowImage} alt='Arrow Left'></img></button>;
+  const rightArrow = currentWidth < 400 ? <></> : <button className={classes.carouselArrows}><img src={rightArrowImage} alt='Arrow Right'></img></button>;
 
   return (
     <div className={classes.background}>
       <WidthWrapper>
-        <div className={classes.items}>
-          <button>
-            <img src={leftArrow} alt="Left Arrow"></img>
-          </button>
-          {coffee.slice(0, sliceSize).map((item) => (
+        <Carousel plugins={['centered',{
+          resolve: arrowsPlugin,
+          options: {
+            arrowLeft: leftArrow,
+            arrowLeftDisabled: <></>,
+            arrowRight: rightArrow,
+            arrowRightDisabled: <></>,
+            addArrowClickHandler: true,
+          }
+        },{
+          resolve: slidesToShowPlugin,
+          options: {
+           numberOfSlides: sliceSize,
+          }
+        }]}>
+          {coffee.map((item) => (
             <Item name={item.name} description={item.description}></Item>
           ))}
-          <button>
-            <img src={rightArrow} alt="Right Arrow"></img>
-          </button>
-        </div>
+        </Carousel>
+        <Dots></Dots>
       </WidthWrapper>
     </div>
   );
